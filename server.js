86eebe7347d797e3e4a3f3d16625da7ecebe110c
Gokul-96 <gokulakrishnan.m96@gmail.com>
2024-02-13 12:29:39 +0530

    commit

--- server.js
+++ server.js
@@ -92,5 +92,5 @@ app.use(errorHandler);
 
 
 const PORT = process.env.PORT || 3001;
-server.listen(PORT, () => console.log("listening on port", PORT));
+server.listen(PORT,'0.0.0.0', () => console.log("listening on port", PORT));
 //node server.js
